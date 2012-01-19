b50fead4650d023de032b1f3e2607e14a08018db
bartek <bartosz.majsak@gmail.com>
2012-01-19 21:46:38 +0100

    Removed @RunAsClient explicit declaration since it's implicitly enabled when using @Deployment(testable=false) mode.

--- web/src/test/java/org/arquillian/example/ui/BeerAdvisorDroneTest.java
+++ web/src/test/java/org/arquillian/example/ui/BeerAdvisorDroneTest.java
@@ -13,7 +13,6 @@ import org.arquillian.example.ui.utils.Deployments;
 import org.arquillian.example.ui.web.Beer;
 import org.arquillian.example.ui.web.BeerAdvisor;
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
@@ -23,7 +22,6 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
 @RunWith(Arquillian.class)
-@RunAsClient
 public class BeerAdvisorDroneTest
 {
    @Deployment(testable = false)
