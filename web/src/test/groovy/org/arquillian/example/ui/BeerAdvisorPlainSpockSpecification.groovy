cfd15b5917a42a62d07507ee405e60b362e66992
Bartosz Majsak <bartosz.majsak@gmail.com>
2012-09-10 15:47:50 +0200

    Added plain Spock specification with WebDriver and Page Objects.

---
+++ web/src/test/groovy/org/arquillian/example/ui/BeerAdvisorPlainSpockSpecification.groovy
@@ -0,0 +1,63 @@
+package org.arquillian.example.ui
+
+import static org.fest.assertions.Assertions.assertThat
+
+import java.net.URL
+import java.util.List
+import java.util.concurrent.TimeUnit
+
+import org.jboss.arquillian.container.test.api.Deployment
+import org.jboss.arquillian.container.test.api.RunAsClient
+import org.jboss.arquillian.drone.api.annotation.Drone
+import org.jboss.arquillian.test.api.ArquillianResource
+import org.jboss.shrinkwrap.api.spec.WebArchive
+import org.junit.Test
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebDriverBackedSelenium;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.arquillian.example.ui.utils.BeersAssert
+import org.arquillian.example.ui.utils.Deployments
+import org.arquillian.example.ui.web.Beer
+import org.arquillian.example.ui.web.BeerAdvisorPage
+import spock.lang.*
+
+/**
+ *
+ * Plain Spock acceptance test for Beer Advisor leveraging WebDriver and its
+ * implementation of Page Objects.
+ */
+@Ignore("This test expects that application is up and running. Available under predefined URL.")
+class BeerAdvisorPlainSpockSpecification extends Specification
+{
+   @Shared def driver
+   @Shared def baseUrl
+
+   def setupSpec()
+   {
+      driver = new FirefoxDriver();
+      baseUrl = "http://localhost:8080/beer-advisor"
+   }
+
+   @Test
+   def "Finding strongest beer using search criteria"()
+   {
+      given: "I'm on the main page"
+         def beerAdvisor = new BeerAdvisorPage(driver, baseUrl)
+         def strongestBeerSearchCriteria = "strongest"
+         def expectedBeerName = "End of history"
+
+      when: "I enter '${strongestBeerSearchCriteria}' as the search criteria and click on the beer"
+         def beers = beerAdvisor.searchFor strongestBeerSearchCriteria
+
+      then: "I should see the strongest beer ever created called ${expectedBeerName}"
+         assertThat(beers).hasSize(1)
+         beers.get(0).shouldBeNamed(expectedBeerName)
+   }
+
+   def cleanupSpec()
+   {
+      driver.close();
+   }
+
+}
