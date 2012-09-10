7231057751bbfe78f8c6eec962f74e18e21a62a2
Aslak Knutsen <aslak@redhat.com>
2012-09-10 21:45:39 +0200

    Add ArquillianEnricher JUnit Rule
    Thucydides does not expose TestInstance in it's StepListener
    so we need a JUnit Rule to access the instance and trigger Before/After
    events for Arquillian Enrichment support.

---
+++ thucydides-tests/src/test/java/org/arquillian/example/thucydides/ArquillianEnricher.java
@@ -0,0 +1,29 @@
+package org.arquillian.example.thucydides;
+
+import org.jboss.arquillian.test.spi.LifecycleMethodExecutor;
+import org.jboss.arquillian.test.spi.TestRunnerAdaptor;
+import org.junit.rules.MethodRule;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.Statement;
+
+public class ArquillianEnricher implements MethodRule
+{
+
+   @Override
+   public Statement apply(final Statement base, final FrameworkMethod method, final Object target)
+   {
+      return new Statement() {
+         public void evaluate() throws Throwable {
+            
+            TestRunnerAdaptor adaptor = ArquillianListener.adaptor.get();
+            adaptor.before(target, method.getMethod(), LifecycleMethodExecutor.NO_OP);
+            try {
+               base.evaluate();
+            } finally {
+               adaptor.after(target, method.getMethod(), LifecycleMethodExecutor.NO_OP);
+            }
+         };
+      };
+   }
+
+}
